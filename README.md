9f1ad231bcf38deb07b2b2a72d653549f4fa7dcc
C-B-Fairchild <chris.fairchild@gmail.com>
2015-01-06 13:06:15 -0500

    first commit and README update

---
+++ README.md
@@ -0,0 +1,1 @@
+a brand new Rails app to demonstrate options with 'Rails new'. This app was created without Javascripts.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewOptions::Application.config.secret_key_base = 'ff838f23ed9b1727af1a912379806b549410bb1762aa7b152a2ecc9fac9dc99c54ba7efe2f6946f7527793730341ee99e0d2c85dedcad8eb123ead8f34b49ad9'
